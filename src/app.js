9754d96cf927830a61798044be593ea8f8bb5d07
Eduardo Stecca <eduardo.stecca@unesp.br>
2021-01-06 12:07:01 -0300

    Criando inserir

--- src/app.js
+++ src/app.js
@@ -2,11 +2,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const handlebars = require('handlebars');
+const handlebars = require('express-handlebars');
 const app = express();
 
+//Template Engine
+app.engine("handlebars", handlebars({defaultLayout: 'main'}));
+app.set('view engine','handlebars');
 
 //Iniciar Servidor
 app.listen(3000,function(req,res){
     console.log('Servidor Iniciado!');
 });
+
+//Rotas e templates
+app.get("/:id?",function(req,res){
+    res.render('index',{id:req.params.id});
+})
